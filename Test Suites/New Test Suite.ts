1202f690e73accaf5902f664c282d89620be4a9d
jeremy.andreas <jeremy.andreas@09-05-0038-0222.ad-ins.com>
2022-04-12 13:12:14 +0700

    fIRST COMMIT

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>93424e16-a4dc-458c-8658-2534acd0fa69</testSuiteGuid>
+   <testCaseLink>
+      <guid>d1dbc974-dced-4c41-b551-bf54d68c5cb5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TEST</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
